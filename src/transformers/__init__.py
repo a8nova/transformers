c4de68f01aaf2f3363a475258d57b06a2107fc36
Matt <rocketknight1@gmail.com>
2024-01-24 14:55:58 +0000

    Fixes to imports/docstrings

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -4071,7 +4071,7 @@ else:
 
     _import_structure["models.idefics"].extend(
         [
-            "TFIDEFICS_PRETRAINED_MODEL_ARCHIVE_LIST",
+            "TF_IDEFICS_PRETRAINED_MODEL_ARCHIVE_LIST",
             "TFIdeficsForVisionText2Text",
             "TFIdeficsModel",
             "TFIdeficsPreTrainedModel",
@@ -8287,7 +8287,7 @@ if TYPE_CHECKING:
             TFHubertPreTrainedModel,
         )
         from .models.idefics import (
-            TFIDEFICS_PRETRAINED_MODEL_ARCHIVE_LIST,
+            TF_IDEFICS_PRETRAINED_MODEL_ARCHIVE_LIST,
             TFIdeficsForVisionText2Text,
             TFIdeficsModel,
             TFIdeficsPreTrainedModel,
