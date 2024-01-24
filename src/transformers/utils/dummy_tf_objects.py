27adac023867fdcb5e46b56b3ab6c9b7b6299023
Matt <rocketknight1@gmail.com>
2024-01-24 14:43:28 +0000

    make fix-copies, make style, remove autotranslated files

--- src/transformers/utils/dummy_tf_objects.py
+++ src/transformers/utils/dummy_tf_objects.py
@@ -1618,6 +1618,30 @@ class TFHubertPreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["tf"])
 
 
+TFIDEFICS_PRETRAINED_MODEL_ARCHIVE_LIST = None
+
+
+class TFIdeficsForVisionText2Text(metaclass=DummyObject):
+    _backends = ["tf"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["tf"])
+
+
+class TFIdeficsModel(metaclass=DummyObject):
+    _backends = ["tf"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["tf"])
+
+
+class TFIdeficsPreTrainedModel(metaclass=DummyObject):
+    _backends = ["tf"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["tf"])
+
+
 TF_LAYOUTLM_PRETRAINED_MODEL_ARCHIVE_LIST = None
 
 
